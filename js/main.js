f5f696073b05494da8bb3fdfc90eb8e211a73168
Kwlad1k <vlad-kad00@mail.ru>
2020-10-03 22:57:19 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//slider
 new WOW().init();
 var mySwiper = new Swiper('.swiper-container', {
   pagination: {
@@ -7,3 +8,17 @@ var mySwiper = new Swiper('.swiper-container', {
     clickable: true
   },
 });
+//smooth scrolling
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+    const blockId = anchor.getAttribute('href')
+    document.querySelector('' + blockId).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+
+  })
+}
